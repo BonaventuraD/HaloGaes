f19501230daa4b123471d6b023060a95d65d509f
jftsnb <92220168+jftsnb@users.noreply.github.com>
2021-10-10 05:26:24 +0800

    Add files via upload

---
+++ views/index4.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js"></script>
+    
+        <title>halogaes</title>
+        <style>
+            html {
+                scroll-behavior: smooth;
+            }
+        </style>
+    
+    <body>
+        <div class="container">
+            <nav class="navbar navbar-expand-sm bg-dark navbar-dark fixed-top">
+            <div class="container">
+                <a class="navbar-brand" href="#">Logo</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse justify-content-end" id="collapsibleNavbar">
+    
+                    <ul class="navbar-nav">
+    
+                        <li class="nav-item" id="spasi">
+                            <a class="nav-link" href="index2.html">Destination</a>
+                        </li>
+                        <li class="nav-item" id="spasi">
+                            <a class="nav-link" href="index3.html">Curator</a>
+                        </li>
+                        <li class="nav-item" id="spasi">
+                            <a class="nav-link" href="index4.html">Account</a>
+                        </li>
+    
+                    </ul>
+    
+                </div>
+            </div>
+            </nav>
+        </div>
+        
+    
+        <div class="container1">
+            <link rel="stylesheet" href="about.css">
+            <h1>My Account</h1>
+        </div>
+        <div class="profile">
+            <h3>Account Name: </h3>
+            <h3>Email: </h3>
+            <h3>Displayed name: </h3>
+        </div>
+        <div class="profilepic">
+            </div>
+        <div class="info">
+            <h2>Preference list</h2>
+        </div>
+        
+        <div class=topcard>
+            <div class="card" style="width: 18rem;">
+                <img src="##" class="card-img-top" alt="...">
+                <div class="card-body">
+                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+              </div>
+              <div class="card" style="width: 18rem;">
+                <img src="##" class="card-img-top" alt="...">
+                <div class="card-body">
+                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+              </div>
+              <div class="card" style="width: 18rem;">
+                <img src="##" class="card-img-top" alt="...">
+                <div class="card-body">
+                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+              </div>
+        </div> 
+        
+    </body>
